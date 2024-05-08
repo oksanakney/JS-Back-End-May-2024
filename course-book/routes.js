25f1af8c8e30bdaf6e403e688f55f32e0d477d58
oksanakney <oksanakney@gmail.com>
2024-05-08 17:17:11 +0300

    404 Error Page works

--- course-book/routes.js
+++ course-book/routes.js
@@ -7,4 +7,8 @@ const authController = require('./controllers/authController');
 router.use(homeController);
 router.use('/auth', authController);
 
+router.all('*', (req, res) => {
+    res.render('404');
+})
+
 module.exports = router;
